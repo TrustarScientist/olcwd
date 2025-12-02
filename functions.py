e43216e1ed2c102c878362f49ae712ac6ee772a3
Daniel Godday Oghenetega <89700191+TrustarScientist@users.noreply.github.com>
2025-12-02 16:16:01 +0100

    Add temperature conversion functions in functions.py

---
+++ functions.py
@@ -0,0 +1,65 @@
+
+# def greet(amount, what_to_greet):
+#     for i in range(amount):
+#         print(what_to_greet)
+#         # if what_to_greet == "Hiya":
+#         #     return 0
+#         # else:
+#         #     return 1 
+
+
+# result = greet(10, "Welcome to my party") 
+# print("Function returned:", result)
+
+
+
+
+def c1():
+    F = int(input("Enter temperature in Fahrenheit: "))
+    celsius = (F - 32) * 5/9
+    print(celsius)
+
+
+
+# # to use c2
+# user_input = int(input("Enter temperature in Fahrenheit: "))
+# c2(user_input)
+
+
+def c2(f_value):
+    celsius = (f_value - 32) * 5/9
+    print(celsius)
+
+# # to use c2
+# user_input = int(input("Enter temperature in Fahrenheit: "))
+# c2(user_input)
+
+def c3():
+    F = int(input("Enter temperature in Fahrenheit: "))
+    celsius = (F - 32) * 5/9
+    return celsius
+
+# result = c3()
+# print("Celsius value is:", result)
+
+def c4(f_value):
+    celsius = (f_value - 32) * 5/9
+    return celsius
+
+# # to use c4
+# f_input = int(input("Enter temperature in Fahrenheit: "))
+# celsius_value = c4(f_input)
+# print("Celsius value is:", celsius_value)
+
+
+    
+
+
+
+
+
+
+
+
+
+
